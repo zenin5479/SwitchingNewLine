3d54fdc04f1d91d8b624d1df2eacde51a674be98
zenin5479 <zenin5479@gmail.com>
2025-12-10 21:31:43 +0300

    Update 0.7 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Net;
 using System.Windows.Forms;
 
 namespace SwitchingNewLine
@@ -14,12 +12,53 @@ namespace SwitchingNewLine
 
       private void ButtonTextBox_Click(object sender, System.EventArgs e)
       {
-        
+         string one = "Первая строка:";
+         string two = "Вторая строка:";
+         string three = "Третья строка:";
+         string four = "Четвертая строка:";
+
+         // Вывод значения разных переменных
+         string linefour = string.Format("Значения переменных: double - {0:f4}; int - {1}; bool - {2};", 8.1234, 25, false);
+         TextBoxReader.AppendText(one);
+         TextBoxReader.AppendText(Environment.NewLine);
+         TextBoxReader.AppendText(Environment.NewLine);
+         TextBoxReader.AppendText(two);
+         TextBoxReader.AppendText(Environment.NewLine);
+
+         TextBoxReader.AppendText(Environment.NewLine);
+         TextBoxReader.AppendText(three);
+         TextBoxReader.AppendText(Environment.NewLine);
+
+         TextBoxReader.AppendText(Environment.NewLine);
+         TextBoxReader.AppendText(four);
+         TextBoxReader.AppendText(Environment.NewLine);
+         TextBoxReader.AppendText(linefour);
+         TextBoxReader.AppendText(Environment.NewLine);
+         TextBoxReader.ScrollToCaret();
       }
 
       private void ButtonListBox_Click(object sender, System.EventArgs e)
       {
-         
+         string one = "Первая строка:";
+         string two = "Вторая строка:";
+         string three = "Третья строка:";
+         string four = "Четвертая строка:";
+
+         // Вывод значения разных переменных
+         string linefour = string.Format("Значения переменных: double - {0:f4}; int - {1}; bool - {2};", 8.1234, 25, false);
+
+
+         ListBoxReader.Items.Add(one);
+
+         ListBoxReader.Items.Add(two);
+
+         ListBoxReader.Items.Add(three);
+
+         ListBoxReader.Items.Add(four);
+         ListBoxReader.Items.Add(linefour);
+         // Прокрутка ListBox вниз
+         ListBoxReader.TopIndex = ListBoxReader.Items.Count - 1;
+
       }
 
       private void ButtonRichTextBox_Click(object sender, System.EventArgs e)
@@ -28,48 +67,21 @@ namespace SwitchingNewLine
          string two = "Вторая строка:";
          string three = "Третья строка:";
          string four = "Четвертая строка:";
-        
+
          // Вывод значения разных переменных
          string linefour = string.Format("Значения переменных: double - {0:f4}; int - {1}; bool - {2};", 8.1234, 25, false);
-         TextBoxReader.AppendText(one);
-         TextBoxReader.AppendText(Environment.NewLine);
-        TextBoxReader.AppendText(Environment.NewLine);
-         TextBoxReader.AppendText(two);
-         TextBoxReader.AppendText(Environment.NewLine);
-        
-         TextBoxReader.AppendText(Environment.NewLine);
-         TextBoxReader.AppendText(three);
-         TextBoxReader.AppendText(Environment.NewLine);
-        
-         TextBoxReader.AppendText(Environment.NewLine);
-         TextBoxReader.AppendText(four);
-         TextBoxReader.AppendText(Environment.NewLine);
-         TextBoxReader.AppendText(linefour);
-         TextBoxReader.AppendText(Environment.NewLine);
-         TextBoxReader.ScrollToCaret();
-
-         ListBoxReader.Items.Add(one);
-        
-         ListBoxReader.Items.Add(two);
-        
-         ListBoxReader.Items.Add(three);
-        
-         ListBoxReader.Items.Add(four);
-         ListBoxReader.Items.Add(linefour);
-         // Прокрутка ListBox вниз
-         ListBoxReader.TopIndex = ListBoxReader.Items.Count - 1;
 
          RichTextBoxReader.AppendText(one);
          RichTextBoxReader.AppendText(Environment.NewLine);
-         
+
          RichTextBoxReader.AppendText(Environment.NewLine);
          RichTextBoxReader.AppendText(two);
          RichTextBoxReader.AppendText(Environment.NewLine);
-         
+
          RichTextBoxReader.AppendText(Environment.NewLine);
          RichTextBoxReader.AppendText(three);
          RichTextBoxReader.AppendText(Environment.NewLine);
-         
+
          RichTextBoxReader.AppendText(Environment.NewLine);
          RichTextBoxReader.AppendText(four);
          RichTextBoxReader.AppendText(Environment.NewLine);
