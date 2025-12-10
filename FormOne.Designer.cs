9cea48e0581e150dde3397f325d749de2165e1cf
zenin5479 <zenin5479@gmail.com>
2025-12-10 21:15:29 +0300

    Добавьте файлы проекта.

---
+++ FormOne.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace SwitchingNewLine
+{
+   partial class FormOne
+   {
+      /// <summary>
+      ///  Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      ///  Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      ///  Required method for Designer support - do not modify
+      ///  the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         SuspendLayout();
+         // 
+         // FormOne
+         // 
+         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         ClientSize = new System.Drawing.Size(800, 450);
+         Name = "FormOne";
+         Text = "Переход на новую строку";
+         ResumeLayout(false);
+      }
+
+      #endregion
+   }
+}
